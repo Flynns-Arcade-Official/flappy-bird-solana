d74dacc48f75c644f554d75d5aaadafea7adc292
VLong <vanlongtran2023@gmail.com>
2024-06-11 16:41:47 +0700

    Update UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta property="og:image" content="/Bonk.png"/>
         <meta property="og:title" content="Flappy Bird" />
         <meta property="og:description" content="Flappy Bird Game" />
         <meta property="og:site_name" content="FlappyBird" />
